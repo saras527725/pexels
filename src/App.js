36c29ea0d64b1246b6abf0d20213938493f8d2ab
SARAS KUMAR <saraskumar52772525@gmail.com>
2024-11-17 03:46:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useEffect, useState } from "react";
+// import Navbar from "./components/Navbar";
+// import axios from "axios";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./components/Home";
+// import Saved from "./components/Saved";
+// const App = () => {
+//   const [images, setImages] = useState([]);
+//   const [search, setSearch] = useState("nature");
+//   const [loader, setLoader] = useState(true);
+//   const [saved, setSaved] = useState([]);
 
-function App() {
+//   const API_KEY = "32RbjC9489RT7tkziFOgH7wbZStsOVRHrO5zPRQJIDuhrndl03Opu1DE";
+
+//   useEffect(() => {
+//     const fetchImage = async () => {
+//       const res = await axios.get(
+//         `https://api.pexels.com/v1/search?query=${search}&per_page=80`,
+//         {
+//           headers: {
+//             Authorization: API_KEY,
+//           },
+//         }
+//       );
+//       //  console.log("response from API = ",res.data.photos)
+//       setLoader(false);
+//       setImages(res.data.photos);
+//       console.log(images);
+//     };
+
+//     const data = JSON.parse(localStorage.getItem("Images"))
+//     if(data){
+//       setSaved(data)
+//     }
+
+//     fetchImage();
+//   }, [search]);
+
+//   useEffect(() => {
+//      if(saved.length !==0){
+//       const json = JSON.stringify(saved)
+//       localStorage.setItem("Images",json)
+//      }
+//   }, [saved])
+  
+
+//   console.log("Image is saved ",saved)
+
+//   return (
+//     <>
+//       <Router>
+//         <Navbar setSearch={setSearch} />
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <Home
+//                 images={images}
+//                 loader={loader}
+//                 saved={saved}
+//                 setSaved={setSaved}
+//               />
+//             }
+//           />
+//           <Route
+//             path="/saved"
+//             element={<Saved saved={saved} loader={loader} />}
+//           />
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// };
+
+// export default App;
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Saved from "./components/Saved";
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [search, setSearch] = useState("nature");
+  const [loader, setLoader] = useState(true);
+  const [saved, setSaved] = useState([]);
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.pexels.com/v1/search?query=${search}&per_page=80`,
+          {
+            headers: {
+              Authorization: API_KEY,
+            },
+          }
+        );
+        setLoader(false);
+        setImages(res.data.photos);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+      }
+    };
+
+    const data = localStorage.getItem("Images");
+    if (data) {
+      setSaved(JSON.parse(data));
+    }
+
+    fetchImage();
+  }, [search]);
+
+  useEffect(() => {
+    if (saved.length !== 0) {
+      localStorage.setItem("Images", JSON.stringify(saved));
+    }
+  }, [saved]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar setSearch={setSearch} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              images={images}
+              loader={loader}
+              saved={saved}
+              setSaved={setSaved}
+            />
+          }
+        />
+        <Route
+          path="/saved"
+          element={<Saved saved={saved} loader={loader} />}
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
